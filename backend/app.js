534cc80a973d18c140c122cad762ff95f4e50d61
Francis Parwez <35888072+francisparwez@users.noreply.github.com>
2021-07-13 13:01:54 +0500

    UPDATES 13TH JULY

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 require('dotenv/config')
+
+app.use(cors())
+app.options('*', cors())
+
 const api = process.env.API_URL
 
 // MIDDLEWARE
@@ -11,45 +15,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(morgan('tiny'))
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: [true, 'Count In Stock Is Required'],
-  },
-})
+// ROUTES
+const categoriesRoutes = require('./routes/categories')
+const ordersRoutes = require('./routes/orders')
+const productsRoutes = require('./routes/products')
+const usersRoutes = require('./routes/users')
 
-const Product = mongoose.model('Product', productSchema)
+app.use(`${api}/categories`, categoriesRoutes)
+app.use(`${api}/orders`, ordersRoutes)
+app.use(`${api}/products`, productsRoutes)
+app.use(`${api}/users`, usersRoutes)
 
-app.get(`${api}/products`, async (req, res) => {
-  const productList = await Product.find()
-  if (!productList) {
-    res.status(500).json({ success: false })
-  }
-  res.send(productList)
-})
-
-app.post(`${api}/product`, (req, res) => {
-  console.log(`${api}/product`)
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  })
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct)
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      })
-    })
-})
-
+// DATABASE
 mongoose
   .connect(process.env.CONNECTION_STRING, {
     useUnifiedTopology: true,
